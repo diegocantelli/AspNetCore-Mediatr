4ec9c6d1b6686909202645b72c1859e92bc48f7b
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-29 17:33:52 -0300

    Criação do projeto e criação da classe de domínio - Produtos

---
+++ WebAppMediatr/WebAppMediatr/Domain/Entities/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMediatr.Domain.Entities
+{
+    public class Produto
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public decimal Valor { get; private set; }
+
+        public Produto(string nome, decimal valor)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Valor = valor;
+        }
+    }
+}

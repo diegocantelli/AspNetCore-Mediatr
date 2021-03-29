95f406dadef6e2d554dcdf7cdb12b5615def7bbe
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-29 17:39:09 -0300

    Criação da classe de response do mediatr

---
+++ WebAppMediatr/WebAppMediatr/Domain/Commands/Responses/ResponseProduto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMediatr.Domain.Commands.Responses
+{
+    //Este é o tipo que será retornado ao executar o send do mediatr 
+    public class ResponseProduto
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Date { get; set; }
+    }
+}

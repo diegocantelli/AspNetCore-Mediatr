4a49bde2b54d155122249f772ac76659bf878130
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-30 10:15:32 -0300

    Configurando a controller para fazer a chamada do mediatr

---
+++ WebAppMediatr/WebAppMediatr/Controllers/ProdutosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppMediatr.Domain.Commands.Requests;
+using WebAppMediatr.Domain.Commands.Responses;
+
+namespace WebAppMediatr.Controllers
+{
+    [Route("api/produtos")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        public async Task<ResponseProduto> Cadastro([FromServices]IMediator mediator, [FromBody] RequestProduto requestProduto)
+        {
+            //O send irá enviar para o método handle que receba o tipo RequestProduto e retorne o tipo ResponseProduto
+            return await mediator.Send(requestProduto);
+        }
+    }
+}

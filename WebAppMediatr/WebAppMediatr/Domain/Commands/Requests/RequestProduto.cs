f71d714877260631503994f01a6377522fa072d4
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-30 10:45:49 -0300

    Tipando ou não a interface IRequest

--- WebAppMediatr/WebAppMediatr/Domain/Commands/Requests/RequestProduto.cs
+++ WebAppMediatr/WebAppMediatr/Domain/Commands/Requests/RequestProduto.cs
@@ -9,6 +9,9 @@ namespace WebAppMediatr.Domain.Commands.Requests
 {
     //Existe um handle para cada request, que será encontrado com base nos parâmetros
     //Haverá um handle que retorne Um ResponseProduto e que recebe um RequestProduto
+    //A interface IRequest pode ser tipada ou não. Nos casos de querys, será tipada de acordo com o retorno da query
+    //Nos casos de comandos, não é necessário ter um tipo de retorno associado, da mesma forma o Handle só terá o parâmetro de entrada
+    // e não terá tipo de retorno
     public class RequestProduto: IRequest<ResponseProduto>
     {
         public string Nome { get; set; }

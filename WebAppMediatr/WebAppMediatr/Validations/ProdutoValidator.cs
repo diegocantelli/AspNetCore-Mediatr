48c5477825f47bae0ab4fc47abdfd1cc36b79a44
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-31 11:38:29 -0300

    Criando os validadores para a classe RequestProduto

---
+++ WebAppMediatr/WebAppMediatr/Validations/ProdutoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppMediatr.Domain.Commands.Requests;
+
+namespace WebAppMediatr.Validations
+{
+    public class ProdutoValidator: AbstractValidator<RequestProduto>
+    {
+        public ProdutoValidator()
+        {
+            RuleFor(x => x.Nome).NotEmpty()
+                .WithMessage("O nome não pode estar vazio");
+
+            RuleFor(x => x.Valor).LessThan(0)
+                .WithMessage("O Valor não pode ser negativo");
+        }
+    }
+}

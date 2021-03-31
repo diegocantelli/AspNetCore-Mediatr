910931695cf41792aa991b6e94da32939536ce05
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-31 11:34:13 -0300

    Comentários sobre o uso da interface IRequest

--- WebAppMediatr/WebAppMediatr/Domain/Commands/Requests/RequestProduto.cs
+++ WebAppMediatr/WebAppMediatr/Domain/Commands/Requests/RequestProduto.cs
@@ -12,6 +12,8 @@ namespace WebAppMediatr.Domain.Commands.Requests
     //A interface IRequest pode ser tipada ou não. Nos casos de querys, será tipada de acordo com o retorno da query
     //Nos casos de comandos, não é necessário ter um tipo de retorno associado, da mesma forma o Handle só terá o parâmetro de entrada
     // e não terá tipo de retorno
+    //IRequest<ResponseProduto> -> ResnponseProduto é o que a query deve retornar
+    //Implementar esta interface é importante, pois é através dela que o mediatr irá descobrir o handler associado a ela.
     public class RequestProduto: IRequest<ResponseProduto>
     {
         public string Nome { get; set; }

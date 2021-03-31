971677f3a43fd230fdf80ddf1d8140fb95b00db9
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-31 11:53:44 -0300

    Implementando a classe de pipeline behavior

---
+++ WebAppMediatr/WebAppMediatr/PipelineBehaviors/ValidationBehavior.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAppMediatr.PipelineBehaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest: IRequest<TResponse>
+    {
+        //Irá armazenar todas as validações registradas no container de dependências para este Request
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var failures = _validators
+                .Select(x => x.Validate(context))
+                .SelectMany(x => x.Errors)
+                .Where(x => x != null)
+                .ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+
+            return next();
+        }
+    }
+}
